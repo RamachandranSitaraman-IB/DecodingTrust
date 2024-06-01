d8d719807641c32d398c18bee2da70ef231d8c50
RamachandranSitaraman-IB <sitaraman@styrk.ai>
2024-06-01 16:36:10 -0700

    Trying out llama-2-7b-chat

--- src/dt/main.py
+++ src/dt/main.py
@@ -1,3 +1,4 @@
+
 from typing import Union, Dict
 
 import hydra
@@ -6,8 +7,6 @@ from dt.configs.configs import BaseConfig
 from omegaconf import OmegaConf, DictConfig
 from hydra.core.config_store import ConfigStore
 from dt.summarize import summarize_results
-from dataclasses import dataclass
-from configs.configs import *
 
 PERSPECTIVES = {
     "stereotype": "dt.perspectives.stereotype.bias_generation",
@@ -22,64 +21,26 @@ PERSPECTIVES = {
 }
 
 
-@dataclass
-class AppConfig:
-    pass
-
-@dataclass
-class SlurmConfig:
-    pass
-
-@dataclass
-class JoblibConfig:
-    pass
-
-# Instantiate the ConfigStore and register your configurations
 cs = ConfigStore.instance()
-cs.store( name="app_config", node=AppConfig)
-cs.store( name="slurm_config", node=SlurmConfig)
-cs.store( name="joblib_config", node=JoblibConfig)
-#cs.store(name="adv_glue", node=AdvGLUEConfig)
-cs.store(group="advglue", name="default", node=AdvGLUEConfig)
-cs.store(group="toxicity", name="default", node=ToxicityConfig)
+cs.store(name="config", node=BaseConfig)
+cs.store(name="slurm_config", node=BaseConfig)
+cs.store(name="joblib_config", node=BaseConfig)
 
-# Using the new config group feature
-@hydra.main(config_path="configs", config_name="app_config", version_base="1.2")
-def main(raw_config: DictConfig) -> None:
-    print("Configuration:", raw_config)
-    config = raw_config
 
-    # if not isinstance(config, BaseConfig):
-    if not isinstance(config, DictConfig):
+@hydra.main(config_path="configs", config_name="config", version_base="1.2")
+def main(raw_config: Union[DictConfig, Dict]) -> None:
+    # The 'validator' methods will be called when you run the line below
+    config: BaseConfig = OmegaConf.to_object(raw_config)
+
+    if not isinstance(config, BaseConfig):
         raise ValueError(f"Wrong type of configuration generated: {type(config)}")
 
     for name, module_name in PERSPECTIVES.items():
         if getattr(config, name) is not None:
-            print("modulename", module_name)
             perspective_module = import_module(module_name)
             perspective_module.main(config)
 
     summarize_results()
-
-
-# @hydra.main(config_path="configs", config_name="config", version_base="1.2")
-# def main(raw_config: Union[DictConfig, Dict]) -> None:
-#     # The 'validator' methods will be called when you run the line below
-#     print("RAWConfig", raw_config)
-#     #config: BaseConfig = OmegaConf.to_object(raw_config)
-#     config = raw_config
-#
-#     #if not isinstance(config, BaseConfig):
-#     if not isinstance(config, DictConfig):
-#         raise ValueError(f"Wrong type of configuration generated: {type(config)}")
-#
-#     for name, module_name in PERSPECTIVES.items():
-#         if getattr(config, name) is not None:
-#             print("modulename", module_name)
-#             perspective_module = import_module(module_name)
-#             perspective_module.main(config)
-#
-#     summarize_results()
 
 
 if __name__ == "__main__":
