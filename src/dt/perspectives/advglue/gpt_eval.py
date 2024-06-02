d1de558e084bcafc48adc424dce945075009c369
RamachandranSitaraman-IB <sitaraman@styrk.ai>
2024-06-01 19:03:16 -0700

    Trying out llama-2-7b-chat

--- src/dt/perspectives/advglue/gpt_eval.py
+++ src/dt/perspectives/advglue/gpt_eval.py
@@ -284,7 +284,7 @@ def main(OPTS):
         elif OPTS.advglue.data_file:
             # Use HF dataset
             assert OPTS.advglue.data_file in ["alpaca", "vicuna", "stable-vicuna"]
-            datasets = load_dataset("AI-Secure/DecodingTrust", name="adv-glue-plus-plus")
+            datasets = load_dataset("DecodingTrust", name="adv-glue-plus-plus")
             datasets = datasets.filter(lambda example: example["model"] == OPTS.advglue.data_file)
         else:
             datasets = None
