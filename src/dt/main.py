a29576c182cb73ff83081fd451a19bf86679fd4f
RamachandranSitaraman-IB <sitaraman@styrk.ai>
2024-06-01 18:50:28 -0700

    Trying out llama-2-7b-chat

--- src/dt/main.py
+++ src/dt/main.py
@@ -32,7 +32,7 @@ def main(raw_config: Union[DictConfig, Dict]) -> None:
     # The 'validator' methods will be called when you run the line below
     print("RawConfig", raw_config)
     config: BaseConfig = OmegaConf.to_object(raw_config)
-
+    print("BaseConfig", config)
     if not isinstance(config, BaseConfig):
         raise ValueError(f"Wrong type of configuration generated: {type(config)}")
 
