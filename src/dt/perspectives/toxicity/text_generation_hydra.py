75bc6aa1b59f29b863bcc550210393f530c828d6
RamachandranSitaraman-IB <sitaraman@styrk.ai>
2024-06-11 20:18:24 -0700

    Trying out llama-2-7b-chat

--- src/dt/perspectives/toxicity/text_generation_hydra.py
+++ src/dt/perspectives/toxicity/text_generation_hydra.py
@@ -19,31 +19,34 @@ def load_prompts(data):
 
 
 def main(OPTS):
+    evaluate = False
+    print("text_generation_hydra.py main", "Setting evaluate to ", evaluate)
     args = OPTS
 
-    constructor = MessageConstructor(PROMPTS[args.toxicity.template]['sys_prompt'], PROMPTS[args.toxicity.template]['task_desc'])
+    if evaluate:
+        constructor = MessageConstructor(PROMPTS[args.toxicity.template]['sys_prompt'], PROMPTS[args.toxicity.template]['task_desc'])
 
-    openai.api_key = args.key
-    data = args.toxicity.data_file
-    dataset = load_prompts(data)
-    # generator = Chat(args.model)
-    with TemporaryDirectory(dir="./.cache") as dirname:
-        generator = Chat.from_helm(args, cache=dirname)
-        result = generator.do_generation(dataset, constructor, n=args.toxicity.n, t=args.toxicity.t, max_tokens=args.toxicity.max_tokens,
-                                         dry_run=args.dry_run)
-        print("cost:", result[0][0])
-        print("#/ prompt tokens:", result[0][1])
-        print("#/ completion tokens:", result[0][2])
-        ensure_dir(args.toxicity.out_file)
-        print(f"Saving to {args.toxicity.out_file}")
+        openai.api_key = args.key
+        data = args.toxicity.data_file
+        dataset = load_prompts(data)
+        # generator = Chat(args.model)
+        with TemporaryDirectory(dir="./.cache") as dirname:
+            generator = Chat.from_helm(args, cache=dirname)
+            result = generator.do_generation(dataset, constructor, n=args.toxicity.n, t=args.toxicity.t, max_tokens=args.toxicity.max_tokens,
+                                             dry_run=args.dry_run)
+            print("cost:", result[0][0])
+            print("#/ prompt tokens:", result[0][1])
+            print("#/ completion tokens:", result[0][2])
+            ensure_dir(args.toxicity.out_file)
+            print(f"Saving to {args.toxicity.out_file}")
 
-        os.makedirs(os.path.dirname(args.toxicity.out_file), exist_ok=True)
-        with open(args.toxicity.out_file, "w") as f:
-            for x in result[1]:
-                res = {"continuation": x[1], "isbanned": x[2], "prompt": x[3], "all_text": x[3] + x[1],
-                       "response": x[-1],
-                       "message": x[0]}
-                f.write(json.dumps(res) + "\n")
+            os.makedirs(os.path.dirname(args.toxicity.out_file), exist_ok=True)
+            with open(args.toxicity.out_file, "w") as f:
+                for x in result[1]:
+                    res = {"continuation": x[1], "isbanned": x[2], "prompt": x[3], "all_text": x[3] + x[1],
+                           "response": x[-1],
+                           "message": x[0]}
+                    f.write(json.dumps(res) + "\n")
 
     Score = namedtuple('Score', ['data_path', 'out_path', 'api', 'prompt_path', 'workers', 'numgen', 'debug', 'model'])
     score_args = Score(
