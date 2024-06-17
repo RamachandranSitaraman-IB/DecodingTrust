6e115ec62f75e57a88905b95e7654197044f8b89
RamachandranSitaraman-IB <sitaraman@styrk.ai>
2024-06-17 01:57:51 -0700

    Trying out llama-2-7b-chat

--- src/dt/configs/configs.py
+++ src/dt/configs/configs.py
@@ -1,5 +1,4 @@
-#from helm.proxy.clients.huggingface_model_registry import ModelLoader, WeightType
-from helm.
+from helm.proxy.clients.huggingface_model_registry import ModelLoader, WeightType
 from dataclasses import dataclass, field
 from typing import Optional, List, Union
 from enum import Enum, auto
